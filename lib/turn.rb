853ececda08ea8f8961e464313eb50c4407037e1
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2017-12-07 15:07:37 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,3 @@
-# Define the variable board below.
-board = [" "," "," "," "," "," "," "," "," "]
-
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
