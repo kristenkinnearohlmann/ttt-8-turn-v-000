39401b78b8d7ee840c52e49f1665707e19bd239e
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2017-12-07 14:55:01 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,37 @@
+# Define display_board that accepts a board and prints
+# out the current state.
+def display_board(board)
+  separator_row = "-----------"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts separator_row
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts separator_row
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(input)
+  input.to_i-1
+end
+
+def move(board,index,character="X")
+  board[index] = character
+end
+
+# code your #position_taken? method here!
+def position_taken?(board,index)
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    false
+  elsif (board[index] == "X" || board[index] == "O")
+    true
+  end
+end
+
+# code your #valid_move? method here
+def valid_move?(board,index)
+  if (index.between?(0,8) && !position_taken?(board,index))
+    true
+  else
+    false
+  end
+end
